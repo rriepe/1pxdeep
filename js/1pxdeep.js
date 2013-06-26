27ac991fca054c639f21946500b2c55fbaaa7f2d
Rex Riepe <rex.riepe@tivix.com>
2013-06-26 13:05:58 -0600

    usability tweaks, new scheme.less

--- js/1pxdeep.js
+++ js/1pxdeep.js
@@ -1,52 +1,10 @@
-
-
 $(document).ready(function() {
-
-    $('#demo_form').submit(function(e){
-
-        var color = $('#scheme_color').val();
-
-        if ($('#tetrad').is(':checked')) {
-            var wheel_pos1 = '30';
-            var wheel_pos2 = '180';
-            var wheel_pos3 = '210';
-        } else if ($('#triad').is(':checked')) {
-            var wheel_pos1 = '120';
-            var wheel_pos2 = '240';
-            var wheel_pos3 = '0';
-        } else if ($('#complement').is(':checked')) {
-            var wheel_pos1 = '180';
-            var wheel_pos2 = '0';
-            var wheel_pos3 = '180';
-        } else if ($('#monochrome').is(':checked')) {
-            var wheel_pos1 = '8';
-            var wheel_pos2 = '352';
-            var wheel_pos3 = '0';
-        } else {
-            var wheel_pos1 = '45';
-            var wheel_pos2 = '315';
-            var wheel_pos3 = '180';
-        }
-
-        less.modifyVars({
-            '@seed-color': color,
-            '@wheel_pos1': wheel_pos1,
-            '@wheel_pos2': wheel_pos2,
-            '@wheel_pos3': wheel_pos3
-        });
-
-        updateColorValues();
-
-        return false;
-    });
-
-
-    $('#demo_form').show();
+    //$('#demo_form').show();
 
     $('#scheme_color').minicolors({
         animationSpeed: 100,
         animationEasing: 'swing',
-        change: null,
+        change: enableColorButton,
         changeDelay: 0,
         control: 'hue',
         defaultValue: '',
@@ -62,16 +20,66 @@ $(document).ready(function() {
         textfield: true,
         theme: 'bootstrap'
     });
+
+    updateColorValues();
+});
+
+$('#toggle_navbar').click(function() {
+    $('#main_navbar').toggle();
+});
+
+$('a[href="#"]').click(function() {
+    return false;
+});
+
+$('#scheme_header').click(function() {
+    $('#scheme_color').focus();
+});
+
+$('#demo_form').change(function(e){
+    enableColorButton();
+});
+
+$('#demo_form').submit(function(e){
+
+    $('#change_color').val('Scheming...');
+
+    var color = $('#scheme_color').val();
+
+    if ($('#tetrad').is(':checked')) {
+        var wheel_pos1 = '30';
+        var wheel_pos2 = '180';
+        var wheel_pos3 = '210';
+    } else if ($('#triad').is(':checked')) {
+        var wheel_pos1 = '120';
+        var wheel_pos2 = '240';
+        var wheel_pos3 = '0';
+    } else if ($('#complement').is(':checked')) {
+        var wheel_pos1 = '180';
+        var wheel_pos2 = '0';
+        var wheel_pos3 = '180';
+    } else if ($('#monochrome').is(':checked')) {
+        var wheel_pos1 = '8';
+        var wheel_pos2 = '352';
+        var wheel_pos3 = '0';
+    } else {
+        var wheel_pos1 = '45';
+        var wheel_pos2 = '315';
+        var wheel_pos3 = '180';
+    }
+
+    less.modifyVars({
+        '@seed-color': color,
+        '@wheel_pos1': wheel_pos1,
+        '@wheel_pos2': wheel_pos2,
+        '@wheel_pos3': wheel_pos3
+    });
+
     updateColorValues();
 
-    $('a[href="#"]').click(function() {
-        return false;
-    });
+    resetColorButton();
 
-    $('#scheme_header').click(function() {
-        $('#scheme_color').focus();
-    });
-
+    return false;
 });
 
 $('#swatches input').focus(function() {
@@ -90,6 +98,15 @@ function updateColorValues() {
         var hex_bg_color = rgb2hex(bg_color);
         $this.find('input').val(hex_bg_color);
     });
+}
+
+function enableColorButton() {
+    $('#change_color').removeClass('disabled');
+}
+
+function resetColorButton() {
+    $('#change_color').addClass('disabled');
+    $('#change_color').val('Scheme now');
 }
 
 function rgb2hex(rgb) {
